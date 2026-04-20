8c8fcca3cc0a135964c2dc037829e10ff0c4358d
Nagaraj Bhutali <4al24cs131@aiet.org.in>
2026-04-21 00:36:18 +0530

    Add files via upload

---
+++ ColorSearch.java
@@ -0,0 +1,28 @@
+package oneB1;
+
+import java.util.*;
+
+public class ColorSearch {
+    public static void main(String[] args) {
+
+        // Creating ArrayList
+        ArrayList<String> colors = new ArrayList<>();
+
+        // Adding colors
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors.add("Red");
+        colors.add("Black");
+
+        // Display colors
+        System.out.println("Colors in the list: " + colors);
+
+        // Searching for "Red"
+        if (colors.contains("Red")) {
+            System.out.println("Red color is available in the list.");
+        } else {
+            System.out.println("Red color is NOT available in the list.");
+        }
+    }
+}
