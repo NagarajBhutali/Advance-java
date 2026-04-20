15bbed7dc5d8f050578b54ca730c874fb9847452
Nagaraj Bhutali <4al24cs131@aiet.org.in>
2026-04-21 00:40:43 +0530

    Add files via upload

---
+++ SortColors.java
@@ -0,0 +1,27 @@
+package oneB3;
+
+import java.util.*;
+
+public class SortColors {
+    public static void main(String[] args) {
+
+        // Create ArrayList
+        ArrayList<String> colors = new ArrayList<>();
+
+        // Add colors
+        colors.add("Blue");
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors.add("Black");
+
+        // Display original list
+        System.out.println("Original List: " + colors);
+
+        // Sort the ArrayList
+        Collections.sort(colors);
+
+        // Display sorted list
+        System.out.println("Sorted List: " + colors);
+    }
+}
